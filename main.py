06cc299ab32215ab77004953392b478205468eb9
Cerasela-b <cerasela.barlaboi@gmail.com>
2025-03-19 10:51:59 +0200

    Changed main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from product import Product
 from product_manager import ProductManager
 
-product1 = Product("Bag", 1500, 80)
-product2 = Product("Shoes", 1700, 20)
-product3 = Product("Belt", 100, 150)
-product4 = Product("Jeans", 500, 30)
+product1 = Product("Bag", 1500, 95)
+product2 = Product("Shoes", 1700, 34)
+product3 = Product("Belt", 100, 200)
+product4 = Product("Jeans", 500, 50)
 
 product_manager = ProductManager()
 
@@ -13,8 +13,8 @@ product_manager.add_product(product2)
 product_manager.add_product(product3)
 product_manager.add_product(product4)
 
-print("All Products in Inventory:")
-print(product_manager.display_all_products())
+#print("All Products in Inventory:")
+#print(product_manager.display_all_products())
 
-total_value = product_manager.total_inventory_value()
-print(f"Total Inventory Value: {total_value} RON")
+#total_value = product_manager.total_inventory_value()
+#print(f"Total Inventory Value: {total_value} RON")
