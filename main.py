1ff36e806c61689b0cb6a515c896995e773dba4e
Cerasela-b <cerasela.barlaboi@gmail.com>
2025-03-19 10:44:26 +0200

    Added Cart class to handle customer cart functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 product1 = Product("Bag", 1500, 80)
 product2 = Product("Shoes", 1700, 20)
@@ -7,6 +8,7 @@ product3 = Product("Belt", 100, 150)
 product4 = Product("Jeans", 500, 30)
 
 product_manager = ProductManager()
+cart = Cart()
 
 product_manager.add_product(product1)
 product_manager.add_product(product2)
@@ -17,4 +19,8 @@ print("All Products in Inventory:")
 print(product_manager.display_all_products())
 
 total_value = product_manager.total_inventory_value()
-print(f"Total Inventory Value: {total_value} RON")
+print(f"Total Inventory Value: ${total_value}")
+
+print("Products in the Cart:")
+print(cart.display_cart())  
+print(f"Total Price: ${cart.calculate_total()}")
